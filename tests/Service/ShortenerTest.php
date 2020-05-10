0154e09f07fb34bba55a9f15792f1b57bff6c991
Konata69 <shaykinspicewolf@gmail.com>
2020-05-10 18:33:31 +0400

    shortener test existed case

--- tests/Service/ShortenerTest.php
+++ tests/Service/ShortenerTest.php
@@ -6,6 +6,7 @@ use App\DTO\UrlDTO;
 use App\Entity\Url;
 use App\Service\Shortener;
 use Doctrine\ORM\EntityManager;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ShortenerTest extends WebTestCase
@@ -31,6 +32,9 @@ class ShortenerTest extends WebTestCase
         $this->shortener = self::$container->get(Shortener::class);
     }
 
+    /**
+     * @throws Exception
+     */
     public function testShort(): void
     {
         $urlStr = "http://www.example.com";
@@ -41,6 +45,30 @@ class ShortenerTest extends WebTestCase
 
         $this->assertEquals($urlStr, $url->getUrl());
         $this->assertRegExp('/[0-9a-zA-Z]{'.$length.'}/', $url->getHash());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testShortExisted(): void
+    {
+        $urlStr = "http://www.example-existed.com";
+        $hash = "abcdefgh";
+
+        $dto = new UrlDTO($urlStr);
+
+        $url = new Url($urlStr);
+        $url->setHash($hash);
+
+        $this->entityManager->persist($url);
+        $this->entityManager->flush();
+        $url = null;
+
+        $this->shortener->short($dto);
+        $url = $this->entityManager->getRepository(Url::class)->findOneBy(['url' => $urlStr]);
+
+        $this->assertEquals($urlStr, $url->getUrl());
+        $this->assertEquals($hash, $url->getHash());
     }
 
     protected function tearDown(): void
